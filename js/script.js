26a02bb55b23c33eaa0a65f2f23b306b26b03c8a
thekosuke <thekosuke929@gmail.com>
2018-08-25 23:37:58 -0700

    Long tap

--- js/script.js
+++ js/script.js
@@ -8,6 +8,30 @@ window.onbeforeunload = function () {
 
 
 /**************************************************
+    DISABLE CONTEXT MENU ON LONG TAPS ON MOBILE
+**************************************************/
+
+    function absorbEvent_(event) {
+      var e = event || window.event;
+      e.preventDefault && e.preventDefault();
+      e.stopPropagation && e.stopPropagation();
+      e.cancelBubble = true;
+      e.returnValue = false;
+      return false;
+    }
+
+    function preventLongPressMenu(node) {
+      node.ontouchstart = absorbEvent_;
+      node.ontouchmove = absorbEvent_;
+      node.ontouchend = absorbEvent_;
+      node.ontouchcancel = absorbEvent_;
+    }
+
+    function init() {
+      preventLongPressMenu(document.getElementById('main-page'));
+    }
+
+/**************************************************
     BACK TO TOP
 **************************************************/
 $(document).ready(function() {
