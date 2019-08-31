691058a4fbc6bd88e699f3dfae044bd9c5c9b8d4
thekosuke <thekosuke929@gmail.com>
2019-08-31 23:41:37 +0900

    hover

--- v4_code/js/script.js
+++ v4_code/js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
       $("#moreBar").toggleClass("active");
       $("#moreContent").toggleClass("active");
       $('#overlay').toggleClass("active");
-      
+
       $(".button").toggleClass("active"); /* rotate */
       $(".dot1").toggleClass("active"); /* spiral */
       $(".dot2").toggleClass("active");
@@ -27,4 +27,91 @@ $(document).ready(function () {
       $(".dot4").toggleClass("active");
       $(".dot5").toggleClass("active");
     });
+});
+
+
+/**************************************************
+    PROJECT: HOVER
+**************************************************/
+$(document).ready(function() {
+  $(".coke h2").mouseenter(function() {
+    $("body").css("background-image", "radial-gradient(#F21919, #FF6868)");
+
+    $(".coke h2").css("color", "#FAFAFC");
+    $(".coke h4").css("color", "#FAFAFC");
+    $("#projects h1").css("color", "#FAFAFC");
+
+    $(".top").css("opacity", "0.2");
+    $(".tp").css("opacity", "0.2");
+    $(".italia").css("opacity", "0.2");
+    $("#experience").css("opacity", "0.2");
+    $("#moreBar").css("opacity", "0.2");
+  });
+  $(".coke h2").mouseleave(function() {
+    $("body").css("background-image", "radial-gradient(#F0F0F2, #FAFAFC)");
+
+    $(".coke h2").css("color", "#16161A");
+    $(".coke h4").css("color", "#7E7E80");
+    $("#projects h1").css("color", "#16161A");
+
+    $(".top").css("opacity", "1");
+    $(".tp").css("opacity", "1");
+    $(".italia").css("opacity", "1");
+    $("#experience").css("opacity", "1");
+    $("#moreBar").css("opacity", "1");
+  });
+
+  $(".tp h2").mouseenter(function() {
+    $("body").css("background", "radial-gradient(#2B65D9, #5FDFE9)");
+
+    $(".tp h2").css("color", "#FAFAFC");
+    $(".tp h4").css("color", "#FAFAFC");
+    $("#projects h1").css("color", "#FAFAFC");
+
+    $(".top").css("opacity", "0.2");
+    $(".coke").css("opacity", "0.2");
+    $(".italia").css("opacity", "0.2");
+    $("#experience").css("opacity", "0.2");
+    $("#moreBar").css("opacity", "0.2");
+  });
+  $(".tp h2").mouseleave(function() {
+    $("body").css("background", "radial-gradient(#F0F0F2, #FAFAFC))");
+
+    $(".tp h2").css("color", "#16161A");
+    $(".tp h4").css("color", "#7E7E80");
+    $("#projects h1").css("color", "#16161A");
+
+    $(".top").css("opacity", "1");
+    $(".coke").css("opacity", "1");
+    $(".italia").css("opacity", "1");
+    $("#experience").css("opacity", "1");
+    $("#moreBar").css("opacity", "1");
+  });
+
+  $(".italia h2").mouseenter(function() {
+    $("body").css("background", "radial-gradient(#0D0D0D, #404040)");
+
+    $(".italia h2").css("color", "#FAFAFC");
+    $(".italia h4").css("color", "#FAFAFC");
+    $("#projects h1").css("color", "#FAFAFC");
+
+    $(".top").css("opacity", "0.2");
+    $(".coke").css("opacity", "0.2");
+    $(".tp").css("opacity", "0.2");
+    $("#experience").css("opacity", "0.2");
+    $("#moreBar").css("opacity", "0.2");
+  });
+  $(".italia h2").mouseleave(function() {
+    $("body").css("background", "radial-gradient(#F0F0F2, #FAFAFC)");
+
+    $(".italia h2").css("color", "#16161A");
+    $(".italia h4").css("color", "#7E7E80");
+    $("#projects h1").css("color", "#16161A");
+
+    $(".top").css("opacity", "1");
+    $(".coke").css("opacity", "1");
+    $(".tp").css("opacity", "1");
+    $("#experience").css("opacity", "1");
+    $("#moreBar").css("opacity", "1");
+  });
 });
