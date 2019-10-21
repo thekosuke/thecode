b30cd07a99eaaee4908c2b8399f452c71bdb43f0
thekosuke <thekosuke929@gmail.com>
2019-10-20 20:37:48 -0700

    ready

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(window).load(function() {
   $('#loader').delay(3000).fadeOut("slow");
 });
 
-$(window).ready(function() {
+$(document).ready(function() {
   $('#project-loader').delay(1000).slideUp(1000);
   $('.loading').delay(1000).fadeOut(1000);
 });
