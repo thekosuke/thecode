9b569b6e1d0637917d805878e8833e72e1277261
thekosuke <thekosuke929@gmail.com>
2020-09-20 16:27:30 -0700

    v7

--- js/script.js
+++ js/script.js
@@ -4,3 +4,44 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
+
+/**************************************************
+    REMOVE # FROM URL
+**************************************************/
+$(document).ready(function() {
+  const menuBtn = $('.side-nav li a');
+
+  menuBtn.click(()=>{
+    setTimeout(()=>{
+      removeHash();
+    }, 5);
+  });
+
+  function removeHash(){
+    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+  }
+});
+
+/**************************************************
+    SMOOTH SCROLLING FOR SIDE NAV
+**************************************************/
+window.addEventListener("DOMContentLoaded", () => {
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			const id = entry.target.getAttribute("id");
+			if (entry.intersectionRatio > 0) {
+				document
+					.querySelector(`nav li a[href="#${id}"]`)
+					.parentElement.classList.add("active");
+			} else {
+				document
+					.querySelector(`nav li a[href="#${id}"]`)
+					.parentElement.classList.remove("active");
+			}
+		});
+	});
+
+	document.querySelectorAll("section[id]").forEach((section) => {
+		observer.observe(section);
+	});
+});
