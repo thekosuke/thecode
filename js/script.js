631086b399f9fa2a42ffddb3052cda0eb3ab254e
thekosuke <thekosuke929@gmail.com>
2022-05-09 17:53:37 -0700

    2022 Q2

--- js/script.js
+++ js/script.js
@@ -21,29 +21,3 @@ $(document).ready(function() {
     history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
   }
 });
-
-/**************************************************
-    SMOOTH SCROLLING FOR SIDE NAV
-**************************************************/
-$(document).ready(function() {
-  window.addEventListener("DOMContentLoaded", () => {
-  	const observer = new IntersectionObserver((entries) => {
-  		entries.forEach((entry) => {
-  			const id = entry.target.getAttribute("id");
-  			if (entry.intersectionRatio > 0) {
-  				document
-  					.querySelector(`.side-nav li a[href="#${id}"]`)
-  					.parentElement.classList.add("active");
-  			} else {
-  				document
-  					.querySelector(`.side-nav li a[href="#${id}"]`)
-  					.parentElement.classList.remove("active");
-  			}
-  		});
-  	});
-
-  	document.querySelectorAll("section[id]").forEach((section) => {
-  		observer.observe(section);
-  	});
-  });
-});
